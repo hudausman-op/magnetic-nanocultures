48ce8ccbb480cc0618b9c6bfc147bb97f126cfdd
Huda Usman <husman@andrew.cmu.edu>
2025-09-04 16:35:50 -0400

    Remove MATLAB scripts from main branch to keep repo clean

--- DisplayVelocity_vlocation.m
+++
@@ -1,6 +0,0 @@
-velocvedd==figure(310)
-hold off
-for i=1:length(velocityinfo)
-    scatter(velocityinfo(i).res(:,1),velocityinfo(i).res(:,3),velocityinfo(i).res(:,5))
-    hold on
-end
--- LimitFoI.m
+++
@@ -1,8 +0,0 @@
-function LimitFoI(infmt,outfmt,frames,topl,botR)
-%topl=[xl yl]: top corner of FoI
-%botR=[xr yr]: bottom corner of FoI
-
-for i=1:length(frames);
-    a=imread(sprintf(infmt,frames(i)));
-    imwrite(a(topl(2):botR(2),topl(1):botR(1)),sprintf(outfmt,frames(i)),'tif','compression','none');
-end
